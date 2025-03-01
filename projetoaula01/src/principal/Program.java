c31ebc47667f7f892c4f60c8cf4cebb48b5bab37
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-01 20:49:13 -0300

    primeiro projeto usando java

--- projetoaula01/src/principal/Program.java
+++ projetoaula01/src/principal/Program.java
@@ -1,15 +1,36 @@
 // define o pacote onde a clase está localizada
 package principal;
 
+import entities.Cliente;
+import repositories.ClienteRepository;
+
 // definição da classe
 // public é obrigatório => define a visibilidade da classe
 public class Program {
+	
 	// método 'main' (função) pra executar o projeto
 	public static void main(String[] args) {
 
 		// imprimir mensagem
 		System.out.println("Hello, word");
-
+		
+	 // [classe] [objeto]  [Inicialização] 
+		Cliente cliente = new Cliente();
+		
+		cliente.setIdCliente(1);
+		cliente.setNome("Daniel Vinicius");
+		cliente.setCpf("123.456.789-00");
+		cliente.setEmail("danielvn@gmail.com");
+		
+		System.out.println("\nDADOS DO CLIENTE\n");
+		System.out.println("\nId do cliente: " + cliente.getIdCliente());
+		System.out.println("\nNOME: " + cliente.getNome());
+		System.out.println("\nCPF DO CLIENTE: " + cliente.getCpf());
+		System.out.println("\nEMAIL DO CLIENTE: " + cliente.getEmail());
+		
+		// [classe]           [objeto]              [Inicialização]
+		ClienteRepository clienteRepository = new ClienteRepository();
+		clienteRepository.exportarTxt(cliente);
 	}
 
 }

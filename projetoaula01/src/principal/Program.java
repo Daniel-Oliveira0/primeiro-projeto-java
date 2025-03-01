3fb6c7d101aa178ca826d3eb7bf1adc0bebd60ba
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-01 12:13:10 -0300

    primeiro teste java

---
+++ projetoaula01/src/principal/Program.java
@@ -0,0 +1,15 @@
+// define o pacote onde a clase está localizada
+package principal;
+
+// definição da classe
+// public é obrigatório => define a visibilidade da classe
+public class Program {
+	// método 'main' (função) pra executar o projeto
+	public static void main(String[] args) {
+
+		// imprimir mensagem
+		System.out.println("Hello, word");
+
+	}
+
+}
